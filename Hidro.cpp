50213bbfe45bdae52b8931a3d67e22924887c91c
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2017-04-15 18:05:35 -0300

    Update Hidro.cpp

--- Hidro.cpp
+++ Hidro.cpp
@@ -3,8 +3,36 @@
 #include <iostream>  
 
 using namespace std;  
-void main()  
-{  
-    cout << "Hello, world, from Visual C++!" << endl;  
-}  
+Contexto::Contexto(){
+	valorPorta = 0;
+	tensao = 0;
+	lampada = 0;
+	//planta = NULL;
+};
+void Contexto::medePorta(int){
+	//A implementar
+};
+void Contexto::converteTensao(int){
+	//A implementar
+};
+void Contexto::nomePlanta(string recebido){
+	 //my_planta (recebido);
+	//planta = my_planta;
+};
+void Contexto::setLampada(int numlamp){
+	lampada = numlamp;
+};
+float Contexto::IndicaTensao(){
+	return(tensao);
+};
+int Contexto::getLampada(){
+	return(lampada);
+};
+string Contexto::getPlanta(){
+	return(planta.name);
+};
+
+bool Controle::calculaTudo(){
+	return(true);//returna true se liga lampada ou false se nao liga
+};
 
