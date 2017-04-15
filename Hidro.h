d7fc3da164efbd0cd100479aa0c514d94d9a472a
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2017-04-15 18:05:39 -0300

    Update Hidro.h

--- Hidro.h
+++ Hidro.h
@@ -32,39 +32,4 @@ class Controle : public Contexto{
 	//Ou não para a planta sendo então o codigo base da parte
 	//do hardware
 } control;
- Contexto::Contexto(){
-	valorPorta = 0;
-	tensao = 0;
-	lampada = 0;
-	//planta = NULL;
-};
-void Contexto::medePorta(int){
-	//A implementar
-};
-void Contexto::converteTensao(int){
-	//A implementar
-};
-void Contexto::nomePlanta(string recebido){
-	 //my_planta (recebido);
-	//planta = my_planta;
-};
-void Contexto::setLampada(int numlamp){
-	lampada = numlamp;
-};
-float Contexto::IndicaTensao(){
-	return(tensao);
-};
-int Contexto::getLampada(){
-	return(lampada);
-};
-string Contexto::getPlanta(){
-	return(planta.name);
-};
-
-bool Controle::calculaTudo(){
-	return(true);//returna true se liga lampada ou false se nao liga
-};
-
-
-
 #endif
