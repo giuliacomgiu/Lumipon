e38514af20d605dae251d3261edf920b958696a2
Giulia Ciprandi <giulia.ciprandi@gmail.com>
2017-04-26 20:19:09 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,12 +1,12 @@
 #include "header.h"
 
-unsigned long utc = 1483228800;
 volatile byte FSM_wake = 0;
-byte minPassed = 0;
+static byte FSM_sec = 0;
 
 void setup() {
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
+    
     noInterrupts();
     //Configurando Watchdog. Para zerar, usar &. Para setar, usar |
     /* Clear the reset flag. */
@@ -15,26 +15,71 @@ void setup() {
     WDTCSR  =   (0 << WDE) |(0 << WDP3) | (1 << WDP2) | (1 << WDP1) | (0 << WDP0);
     WDTCSR  |=  (1 << WDIE);
     interrupts();
+    l1.setPlanta("alface");
+    l1.setLampada(1);
+    l1.setTempoPlanta(2);
+    l1.setTempoPercorrido();
+    l1.setTempoPercorrido();
 }
 
 void loop() {
+    static unsigned long utc = 1483228800;
+    float LDRVoltage;
+    byte timeCountData;
+    byte timeCountLDR;
     tm localTime;
     
-    PowerDown();
+    /*How was uC woken? Ext int or WDT?*/
     switch(FSM_wake){
         case SEC_INCR:
+            utc++;
+            Serial.println(localTime.seg);
+            utcToTime(&localTime, &utc);
+            FSM_wake &= ~(1 << SEC_INCR_bit);
+
+            /*Turn on timeCountData and timeCountLDR*/
+            if(timeCountData >= UPDT_RATE){
+                FSM_sec |= (1 << UPDT_DATA_bit);
+                timeCountData = 0;
+            }else if(timeCountLDR >= UPDT_LDR){
+                FSM_sec |= (1 << UPDT_LDR_bit);
+                timeCountLDR = 0;
+            }else if(timeCountLDR >= UPDT_LDR && timeCountData >= UPDT_RATE){
+                FSM_sec |= (1 << UPDT_DATA_bit);
+                FSM_sec |= (1 << UPDT_LDR_bit);
+                timeCountLDR = 0;
+                timeCountData = 0;
+            }
+
+            /*Increment timeCountData and timeCountLDR. Both in mins*/
+            if (localTime.seg == 0){
+                timeCountData++;
+                timeCountLDR++;
+            }
+        break;
+        switch(FSM_sec){
+            case SEC_INCR_AND_UPDT_DATA:
             /*
              * Call utc to time func
-             * If 5 min passed, update data
-             * Else, do nothing. For now
-            */
-            utcToTime(&localTime);
-            FSM_wake &= ~(1 << SEC_INCR_bit);
-            if(localTime.minu - minPassed >= UPDT_RATE){
-                //Get data
-                Serial.print(UPDT_RATE); Serial.println(" minute(s) passed.");
-                minPassed = localTime.minu;
-                }
+             * Update data
+             */
+                FSM_sec &= ~(1 << UPDT_DATA_bit);
+                utcToTime(&localTime, &utc);
+            break;
+            case SEC_INCR_AND_UPDT_LDR:
+                FSM_sec &= ~(1 << UPDT_LDR_bit);
+                LDRVoltageUpdt(&LDRVoltage);
+            break;
+            case SEC_INCR_AND_UPDT_DATA_AND_LDR:
+                FSM_sec &= ~((1 << UPDT_LDR_bit) | (1 << UPDT_DATA_bit));
+                LDRVoltageUpdt(&LDRVoltage);
+            break;
+            default:
+            break;
+        } 
+        default:
+            PowerDown();
         break;
-        };
+        PowerDown();
+    };
 }
